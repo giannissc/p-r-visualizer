5eec67e2516e1ace5739112d780aa8793c108f56
John Skottis <giannis.sc@gmail.com>
2021-01-19 17:12:51 +0000

    Add painting of grid axis lines using the new HashMap Implementation

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 // TODO Implement infinite zoom and pan functionallity. See scroll example and clipBox documentation
 // TODO Configure rustfmt and clippy
-// FIXME At grid dimensions (where width > height) a stack buffer overrun happens
-// FIXME At gird dimensions (where height > width) at certain window sizes additional row are added to the grid (equal to the number of lines height is bigger than width), the pattern is the mirrored and shifted left by one.
-// FIXME Removing ctx.stroke shows grid lines on windowed mode but not maximized. 
-// FIXME Having ctx.stroke the same colour as ctx.fill still shows a hint of the grid lines
-// FIXME Moving the window causes the app to redraw. File a bug for that
+// TODO Merge Grid and GridWidget into one struct
+// TODO Implement Grid axis lines.
+// FIXME Removing ctx.stroke shows grid lines on windowed mode but not maximized.
 
+use std::ffi::IntoStringError;
 // Druid imports
 use druid::widget::{
     Align, Button, Container, CrossAxisAlignment, Flex, Label, List, Padding, RadioGroup, Scroll,
@@ -15,7 +14,7 @@ use druid::{
     theme, AppLauncher, BoxConstraints, Data, Lens, LocalizedString, WidgetExt,
     WindowDesc,
 };
-use druid::{Color, Point, Rect, Size, im::HashMap};
+use druid::{Color, Point, Rect, Size, im::HashMap, kurbo::Line};
 use druid::{
     Env, Event, EventCtx, LayoutCtx, LifeCycle, LifeCycleCtx, MouseButton, PaintCtx, RenderContext,
     UpdateCtx, Widget,
@@ -24,8 +23,8 @@ use druid::{
 //////////////////////////////////////////////////////////////////////////////////////
 // Constants
 //////////////////////////////////////////////////////////////////////////////////////
-const GRID_WIDTH: usize = 40; 
-const GRID_HEIGHT: usize = 50; 
+const GRID_COLUMNS: usize = 500; 
+const GRID_ROWS: usize = 250; 
 const LIGHT_BLUE: Color = Color::from_rgba32_u32(0xA4CBFA); 
 
 //////////////////////////////////////////////////////////////////////////////////////
@@ -50,7 +49,6 @@ struct Grid {
 
 // Application Custom Widgets
 struct GridWidget {
-    
     cell_size: Size,
     color: Color,
 }
@@ -118,7 +116,7 @@ impl GridWidget {
         }
         let row = (p.x / w0) as usize;
         let col = (p.y / h0) as usize;
-        if row >= GRID_WIDTH || col >= GRID_HEIGHT {
+        if row >= GRID_COLUMNS || col >= GRID_ROWS {
             return None;
         }
         Some(GridPos { row, col })
@@ -136,12 +134,6 @@ impl Widget<State> for GridWidget {
                 if e.button == MouseButton::Left {
                     let grid_pos_opt = self.grid_pos(e.pos);
                     grid_pos_opt.iter().for_each(|pos| {
-                        //println!("Event Down {:?}", pos);
-                        let point = Point {
-                            x: self.cell_size.width * pos.row as f64,
-                            y: self.cell_size.height * pos.col as f64,
-                        };
-                        let rect = Rect::from_origin_size(point, self.cell_size);
                         if data.drawing == Interaction::None {
                             if data.grid.storage.contains_key(pos) {
                                 data.grid.storage.remove(pos);
@@ -149,10 +141,19 @@ impl Widget<State> for GridWidget {
                             } else {
                                 data.grid.storage.insert(*pos, GridNodes::Wall);
                                 data.drawing = Interaction::Drawing
-                                
                             }
                         }
+
+                        let point = Point {
+                            x: self.cell_size.width * pos.row as f64,
+                            y: self.cell_size.height * pos.col as f64,
+                        };
+                        let rect = Rect::from_origin_size(point, self.cell_size);
+                        //println!("Event - Position - Invalidation Rectangle: {:?}", point);
+                        //println!("Event - Size - Invalidation Rectangle: {:?}", rect.size());
+                        //println!("Event - Size - Cell: {:?}\n================================", self.cell_size);
                         ctx.request_paint_rect(rect);
+                        //ctx.request_paint();
                     });
                 }
             }
@@ -166,17 +167,22 @@ impl Widget<State> for GridWidget {
                     let grid_pos_opt = self.grid_pos(e.pos);
                     grid_pos_opt.iter().for_each(|pos| {
                         //println!("Event Move: {:?}", *pos);
-                        let point = Point {
-                            x: self.cell_size.width * pos.row as f64,
-                            y: self.cell_size.height * pos.col as f64,
-                        };
-                        let rect = Rect::from_origin_size(point, self.cell_size);
                         if data.drawing == Interaction::Drawing {
                             data.grid.storage.insert(*pos, GridNodes::Wall);
                         } else if data.drawing == Interaction::Erasing {
                             data.grid.storage.remove(pos);
                         }
+
+                        let point = Point {
+                            x: self.cell_size.width * pos.row as f64,
+                            y: self.cell_size.height * pos.col as f64,
+                        };
+                        let rect = Rect::from_origin_size(point, self.cell_size);
+                        //println!("Event - Position - Invalidation Rectangle: {:?}", point);
+                        //println!("Event - Size - Invalidation Rectangle: {:?}", rect.size());
+                        //println!("Event - Size - Cell: {:?}\n================================", self.cell_size);
                         ctx.request_paint_rect(rect);
+                        //ctx.request_paint();
                     });
                 }
             }
@@ -203,7 +209,7 @@ impl Widget<State> for GridWidget {
 
         Size {
             width: width,
-            height: (GRID_HEIGHT as f64 * width) / GRID_WIDTH as f64,
+            height: (GRID_ROWS as f64 * width) / GRID_COLUMNS as f64,
         }
     }
 
@@ -212,14 +218,14 @@ impl Widget<State> for GridWidget {
         let grid_size: Size = ctx.size();
         
         let cell_size = Size {
-            width: grid_size.width.max(grid_size.height) / GRID_WIDTH.max(GRID_HEIGHT) as f64,
-            height: grid_size.width.max(grid_size.height) / GRID_WIDTH.max(GRID_HEIGHT) as f64,
+            width: grid_size.width.max(grid_size.height) / GRID_COLUMNS.max(GRID_ROWS) as f64,
+            height: grid_size.width.max(grid_size.height) / GRID_COLUMNS.max(GRID_ROWS) as f64,
         };
         self.cell_size = cell_size;
+
         //println!("Cell size: {:?}", cell_size);
-
+        
         // Draw grid cells
-
         for (cell_pos, cell_type) in data.grid.storage.iter(){
             let point = Point {
                 x: cell_size.width * cell_pos.row as f64,
@@ -236,12 +242,27 @@ impl Widget<State> for GridWidget {
         }
 
         // Draw grid axis
-        if data.grid.show_grid_axis {
-            //println!("Painting region:{:?}", &ctx.region());
-            //println!("Painting region bounding box:{:?}", &ctx.region().bounding_box());
-            //println!("Painting region size:{:?}", &ctx.size());
+        for row in 1..=GRID_ROWS {
+            let from_point = Point {
+                x: 0.0,
+                y: cell_size.height * row as f64,
+            };
+
+            let size = Size::new(ctx.size().width, self.cell_size.height * 0.05);
+            let rect = Rect::from_origin_size(from_point, size);
+            ctx.fill(rect, &Color::GRAY);
         }
 
+        for column in 1..=GRID_COLUMNS {
+            let from_point = Point {
+                x: cell_size.width * column as f64,
+                y: 0.0,
+            };
+
+            let size = Size::new( self.cell_size.width * 0.05, ctx.size().height);
+            let rect = Rect::from_origin_size(from_point, size);
+            ctx.fill(rect, &Color::GRAY);  
+        }
 
     }
 
@@ -263,7 +284,7 @@ impl Widget<State> for GridWidget {
 fn main() {
     // TODO Arrange for window size to be set so that it fits the number of row, columns, cell_size
     let main_window = WindowDesc::new(make_ui_simple)
-        .window_size((500.0, 1000.0))
+        .window_size((1000.0, 500.0))
         .title(LocalizedString::new("Placement & Routing Experiments"));
     let grid = Grid::new();
     let data = State {
@@ -289,6 +310,6 @@ fn main() {
 ////////////////////////////////////////////////////////////////////////////////////////////////
 
 fn make_ui_simple() -> impl Widget<State> {
-    Flex::column().with_child(GridWidget::new().debug_invalidation())
-    //Flex::column().with_child(GridWidget::new())
+    //Flex::column().with_child(GridWidget::new().debug_invalidation())
+    Flex::column().with_child(GridWidget::new())
 }
